72e2641b16ed2d4897f45ded686ae3d600722690
erikvw <ew2789@gmail.com>
2019-09-22 17:55:21 -0500

    update allowed parent actions for end of study

--- meta_prn/action_items.py
+++ meta_prn/action_items.py
@@ -1,6 +1,7 @@
 from edc_constants.constants import HIGH_PRIORITY, TBD, YES
 from edc_action_item.action_with_notification import ActionWithNotification
 from edc_action_item.site_action_items import site_action_items
+from edc_adverse_event.constants import DEATH_REPORT_ACTION
 
 from .constants import (
     END_OF_STUDY_ACTION,
@@ -15,7 +16,7 @@ class EndOfStudyAction(ActionWithNotification):
     name = END_OF_STUDY_ACTION
     display_name = "Submit End of Study Report"
     notification_display_name = "End of Study Report"
-    parent_action_names = [UNBLINDING_REVIEW_ACTION]
+    parent_action_names = [UNBLINDING_REVIEW_ACTION, DEATH_REPORT_ACTION]
     reference_model = "meta_prn.endofstudy"
     show_link_to_changelist = True
     admin_site_name = "meta_prn_admin"
