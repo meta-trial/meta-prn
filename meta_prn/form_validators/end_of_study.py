5e0b52772a82f1ccf5951d35fc8df1f80258d174
erikvw <ew2789@gmail.com>
2020-01-18 12:43:55 -0600

    add other validator for offschedule_reason, fix field name in formvalidator

--- meta_prn/form_validators/end_of_study.py
+++ meta_prn/form_validators/end_of_study.py
@@ -1,5 +1,5 @@
 from edc_consent.constants import CONSENT_WITHDRAWAL
-from edc_constants.constants import DEAD
+from edc_constants.constants import DEAD, OTHER
 from edc_form_validators import FormValidator
 
 
@@ -11,22 +11,28 @@ class EndOfStudyFormValidator(ValidateDeathReportMixin, FormValidator):
 
         self.validate_death_report_if_deceased()
 
-        self.required_if(DEAD, field="termination_reason", field_required="death_date")
+        self.validate_other_specify(
+            field="offschedule_reason",
+            other_specify_field="other_offschedule_reason",
+            other_stored_value=OTHER,
+        )
+
+        self.required_if(DEAD, field="offschedule_reason", field_required="death_date")
 
         self.required_if(
             CONSENT_WITHDRAWAL,
-            field="termination_reason",
+            field="offschedule_reason",
             field_required="consent_withdrawal_reason",
         )
 
         self.required_if(
             "included_in_error",
-            field="termination_reason",
+            field="offschedule_reason",
             field_required="included_in_error",
         )
 
         self.required_if(
             "included_in_error",
-            field="termination_reason",
+            field="offschedule_reason",
             field_required="included_in_error_date",
         )
