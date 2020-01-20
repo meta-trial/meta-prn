7434be8781ee83f6e23b1ff95c6ee0c90a64dc8c
erikvw <ew2789@gmail.com>
2020-01-20 11:43:32 -0600

    split other option on end_of_study, update validator

--- meta_prn/form_validators/end_of_study.py
+++ meta_prn/form_validators/end_of_study.py
@@ -2,7 +2,7 @@ from edc_consent.constants import CONSENT_WITHDRAWAL
 from edc_constants.constants import DEAD, OTHER
 from edc_form_validators import FormValidator
 
-
+from ..constants import OTHER_RX_DISCONTINUATION
 from .validate_death_report_mixin import ValidateDeathReportMixin
 
 
@@ -11,11 +11,23 @@ class EndOfStudyFormValidator(ValidateDeathReportMixin, FormValidator):
 
         self.validate_death_report_if_deceased()
 
-        self.validate_other_specify(
-            field="offschedule_reason",
-            other_specify_field="other_offschedule_reason",
-            other_stored_value=OTHER,
-        )
+        if self.cleaned_data.get("offschedule_reason"):
+            if self.cleaned_data.get("offschedule_reason").name != OTHER:
+                self.validate_other_specify(
+                    field="offschedule_reason",
+                    other_specify_field="other_offschedule_reason",
+                    other_stored_value=OTHER_RX_DISCONTINUATION,
+                )
+
+            if (
+                self.cleaned_data.get("offschedule_reason").name
+                != OTHER_RX_DISCONTINUATION
+            ):
+                self.validate_other_specify(
+                    field="offschedule_reason",
+                    other_specify_field="other_offschedule_reason",
+                    other_stored_value=OTHER,
+                )
 
         self.required_if(DEAD, field="offschedule_reason", field_required="death_date")
 
